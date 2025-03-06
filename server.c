ed4f19a49e9557da29de8295bcd78bb1924cfc75
wen2ms <1819509111@qq.com>
2025-03-06 22:46:48 +0800

    Feat: add server and client demo

---
+++ server.c
@@ -0,0 +1,81 @@
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main() {
+    int listen_file_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (listen_file_descriptor == -1) {
+        perror("socket");
+        return -1;
+    }
+
+    struct sockaddr_in server_address;
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9999);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    int ret = bind(listen_file_descriptor, (struct sockaddr*)&server_address, sizeof(server_address));
+
+    if (ret == -1) {
+        perror("bind");
+        return -1;
+    }
+
+    ret = listen(listen_file_descriptor, 128);
+
+    if (ret == -1) {
+        perror("listen");
+        return -1;
+    }
+
+    struct sockaddr_in client_address;
+    unsigned int client_address_len = sizeof(client_address);
+
+    int client_file_descriptor = accept(listen_file_descriptor, (struct sockaddr*)&client_address, &client_address_len);
+
+    if (client_file_descriptor == -1) {
+        perror("accept");
+        return -1;
+    }
+
+    char ip[32];
+    printf("Client IP: %s, Port: %d\n", inet_ntop(AF_INET, &client_address.sin_addr.s_addr, ip, sizeof(ip)),
+           ntohs(client_address.sin_port));
+        
+    while (1) {
+        char buffer[1024] = {0};
+        int receive_data_len = recv(client_file_descriptor, buffer, sizeof(buffer), 0);
+
+        if (receive_data_len > 0) {
+            printf("client says before processing: %s\n", buffer);
+
+            for (int i = 0; i < receive_data_len; ++i) {
+                buffer[i] = toupper(buffer[i]);
+            }
+
+            printf("client says after processing: %s\n", buffer);
+
+            ret = send(client_file_descriptor, buffer, receive_data_len, 0);
+            if (ret == -1) {
+                perror("send");
+                return -1;
+            }
+
+        } else if (receive_data_len == 0) {
+            printf("client disconnected...\n");
+            break;
+        } else {
+            perror("recv");
+            return -1;
+        }
+    }
+
+    close(listen_file_descriptor);
+    close(client_file_descriptor);
+
+    return 0;
+}
